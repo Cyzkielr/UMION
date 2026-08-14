c019203d275d6c237d6e4c3f55f62ff2aa240e5c
Cyzkielr <cyzkielr@gmail.com>
2026-08-14 20:29:26 +0800

    update configManager.js

--- js/managers/configManager.js
+++ js/managers/configManager.js
@@ -1,9 +1,32 @@
+const CONFIG_STORAGE_KEY = "umiom-config";
+
+function saveConfig() {
+  localStorage.setItem(
+    CONFIG_STORAGE_KEY,
+    JSON.stringify(state.config)
+  );
+}
+
 function getConfig() {
   return state.config;
 }
 
 function updateConfig(key, value) {
   state.config[key] = value;
+  saveConfig();
+}
+
+function loadSavedConfig() {
+  const saved = localStorage.getItem(CONFIG_STORAGE_KEY);
+  
+  if (!saved) {
+    return
+  }
+  
+  Object.assign(
+    state.config,
+    JSON.parse(saved)
+  );
 }
 
 function getPreferredSpark() {
@@ -25,3 +48,5 @@ function getPreferredRunningStyle() {
 function getBorrowPriority() {
   return state.config.borrowPriority;
 }
+
+loadSavedConfig();
